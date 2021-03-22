5452b7d67cae0ca0c80b703148c0b3cd8f34b50c
Fang <1803967584@qq.com>
2021-03-22 16:34:29 +0800

    CSV工具类的实现

--- src/test/java/com/fxz/csvdemo/CsvDemoApplicationTests.java
+++ src/test/java/com/fxz/csvdemo/CsvDemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.fxz.csvdemo;
 
+import com.fxz.csvdemo.entity.Employee;
+import com.fxz.csvdemo.mapper.EmployeeMapper;
+import com.fxz.csvdemo.util.CSVWriterUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CsvDemoApplicationTests {
 
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
     @Test
     void contextLoads() {
+        Employee employee = employeeMapper.selectById("1337278573614555137");
+        CSVWriterUtils.writeCsv("demos.csv", employee);
     }
 
 }
